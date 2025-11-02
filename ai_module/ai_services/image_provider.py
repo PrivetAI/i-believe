9b3effa6d42a7c6037e43bed600d4a590840bce5
Viktor <viktor@v.v>
2025-11-02 19:05:38 +0700

    up

--- ai_module/ai_services/image_provider.py
+++ ai_module/ai_services/image_provider.py
@@ -3,11 +3,10 @@ from pathlib import Path
 from ai_services.image_providers.fal_client import FalClient
 from ai_services.image_providers.imagen_client import ImagenClient
 from ai_services.image_providers.black_forest_client import BlackForestClient
+from ai_services.image_providers.grok_client import GrokClient
 from ai_services.replicate_client import ReplicateClient
 
 class ImageProvider:
-    """Unified interface для всех image провайдеров"""
-    
     PROVIDERS = {
         "fal": "Fal.ai",
         "imagen": "Google Imagen",
@@ -28,6 +27,8 @@ class ImageProvider:
             return ImagenClient(self.api_key)
         elif self.provider == "black_forest":
             return BlackForestClient(self.api_key)
+        elif self.provider == "grok":
+            return GrokClient(self.api_key)
         elif self.provider == "replicate":
             return ReplicateClient(self.api_key)
         else:
